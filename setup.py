f615c1d28ad3a8c0605ac96dd0b652070e930c72
letang974-blip <letang974@gmail.com>
2025-08-19 12:17:59 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+APP = ['main.py']  # Your entry point script
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'tri.icns',  # Custom icon
+    'plist': {
+        'CFBundleName': 'Photo Organizer',
+        'CFBundleDisplayName': 'Photo Organizer',
+        'CFBundleIdentifier': 'com.yourname.photoorganizer',
+        'CFBundleVersion': '1.0.0',
+        'CFBundleShortVersionString': '1.0.0',
+    }
+}
+
+setup(
+    app=APP,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
